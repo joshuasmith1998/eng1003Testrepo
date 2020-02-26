ca9c9cb3333c83fe2996529929d12fd9092c4c22
Joshua Smith <c3282117@uon.edu.au>
2020-02-26 11:20:08 +1100

    initial commit

--- main.c
+++ main.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 
 int main() {
-    float x;
+    float x; //fff//
     float y;
     printf("Enter a number: ");
     scanf("%f", &x);
     y = (x+2/x-1) ;
     printf("y: %f/n", y);
-    //
     return 0;
 }
